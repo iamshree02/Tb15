72f3273fdff604c3aff2ff1eb42d9cf74ca7446d
iamshree02 <107460692+iamshree02@users.noreply.github.com>
2022-06-15 11:33:05 +0530

    by shrikant

--- Day35_GitHub/src/demo/Shrikant.java
+++ Day35_GitHub/src/demo/Shrikant.java
@@ -4,6 +4,10 @@ public class Shrikant {
 	public void m1() {
 		System.out.println("Code Edited");
 	}
+	
+	public void m2(){
+		System.out.println("Code Edited from GitHub Website--Code merge");
+		}
 public static void main(String[] args) {
 	System.out.println("GitHub DEmo");
 }
